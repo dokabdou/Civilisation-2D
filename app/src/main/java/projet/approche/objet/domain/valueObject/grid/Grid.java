f6cfe07d200f3f43774f6246743418b1e7a6405b
TO <th.theophyl.hoguet@gmail.com>
2023-12-04 09:05:06 +0100

    Refactor Grid class to use char[][] instead of String[][], and add Map<Character, Coordinate> for buildings

--- app/src/main/java/projet/approche/objet/domain/valueObject/grid/Grid.java
+++ app/src/main/java/projet/approche/objet/domain/valueObject/grid/Grid.java
@@ -1,15 +1,19 @@
 package projet.approche.objet.domain.valueObject.grid;
 
+import java.util.Map;
+
 public class Grid {
 
-    private int gridSize;
-    private String[][] grid;
-    public Grid(int gridSize) {
-        this.gridSize = gridSize;
-        this.grid = new String[gridSize][gridSize];
-    }
+	private final int gridSize;
+	private char[][] grid;
+	private Map<Character, Coordinate> buildings;
 
-    public void setGrid(int x, int y, String value) {
-        this.grid[x][y] = value;
-    }
+	public Grid(int gridSize) {
+		this.gridSize = gridSize;
+		this.grid = new char[gridSize][gridSize];
+	}
+
+	public void setGrid(int x, int y, char value) {
+		this.grid[x][y] = value;
+	}
 }
