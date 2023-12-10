9983a3d8ecdd17782b94c7c731a267c03c56869b
TO <th.theophyl.hoguet@gmail.com>
2023-12-10 15:07:21 +0100

    remove useless file section when starting a game

--- app/src/main/java/projet/approche/objet/ui/view/GameStarterView.java
+++ app/src/main/java/projet/approche/objet/ui/view/GameStarterView.java
@@ -12,41 +12,9 @@ import projet.approche.objet.domain.valueObject.game.GameStarter;
 public class GameStarterView extends BorderPane {
 
 	public GameStarterView(Stage stage) {
-		MenuBar menuBar = new MenuBar();
-		Menu fileMenu = new Menu("File");
-
-		MenuItem easyGame = new MenuItem("Load EASY game");
-		MenuItem normalGame = new MenuItem("Load NORMAL game");
-		MenuItem hardGame = new MenuItem("Load HARD game");
-
-		fileMenu.getItems().addAll(
-				easyGame, normalGame, hardGame);
-		menuBar.getMenus().addAll(fileMenu);
-		this.setTop(menuBar);
-
-		easyGame.setOnAction(e -> {
-			startGame(stage, GameStarter.EASY, 10);
-		});
-
-		normalGame.setOnAction(e -> {
-			startGame(stage, GameStarter.NORMAL, 10);
-		});
-
-		hardGame.setOnAction(e -> {
-			startGame(stage, GameStarter.HARD, 10);
-		});
-
 	}
 
 	private static void startGame(Stage stage, GameStarter gs, int gridSize) {
-		App app = new App(gs, gridSize);
-		GameView gameView = new GameView(stage, app);
-		Scene gameScene = new Scene(gameView);
-
-		stage.setTitle(gs.toString());
-		stage.setScene(gameScene);
-		stage.sizeToScene();
-		stage.show();
 	}
 
 }
