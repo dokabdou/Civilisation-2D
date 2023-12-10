1bff7509148e270d7f572dcd07a7ec632e2e003f
TO <th.theophyl.hoguet@gmail.com>
2023-12-10 18:18:16 +0100

    Game Over page Fixes #30

--- app/src/main/java/projet/approche/objet/ui/Main.java
+++ app/src/main/java/projet/approche/objet/ui/Main.java
@@ -2,31 +2,25 @@ package projet.approche.objet.ui;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-import projet.approche.objet.ui.view.GameInfoView;
 import projet.approche.objet.ui.view.GameStarterView;
 
 public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-
-		GameStarterView gameStarterView = new GameStarterView(primaryStage);
-		ImageView imageView = new ImageView(new Image("images/Game.png"));
-		GameInfoView gameInfoView = new GameInfoView(primaryStage, imageView);
-		gameStarterView.setCenter(gameInfoView);
-		gameStarterView.prefWidthProperty().bind(imageView.fitWidthProperty());
-		gameStarterView.prefHeightProperty().bind(imageView.fitHeightProperty());
-
-		primaryStage.setTitle("Approche Objet");
-		primaryStage.setScene(new Scene(gameStarterView));
-		primaryStage.show();
+		restart(primaryStage);
 	}
 
 	public static void main(String[] args) {
 		launch();
 	}
 
+	public static void restart(Stage stage) {
+		GameStarterView gameStarterView = new GameStarterView(stage);
+
+		stage.setTitle("Approche Objet");
+		stage.setScene(new Scene(gameStarterView));
+		stage.show();
+	}
 }
