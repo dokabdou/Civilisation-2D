c0458c92eb3e6c8818f7ace5d2dd9825ca7440fc
TO <th.theophyl.hoguet@gmail.com>
2023-12-10 15:08:14 +0100

    remove unused import

--- app/src/main/java/projet/approche/objet/ui/view/GameStarterView.java
+++ app/src/main/java/projet/approche/objet/ui/view/GameStarterView.java
@@ -1,20 +1,10 @@
 package projet.approche.objet.ui.view;
 
-import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import projet.approche.objet.application.App;
-import projet.approche.objet.domain.valueObject.game.GameStarter;
 
 public class GameStarterView extends BorderPane {
 
 	public GameStarterView(Stage stage) {
 	}
-
-	private static void startGame(Stage stage, GameStarter gs, int gridSize) {
-	}
-
 }
